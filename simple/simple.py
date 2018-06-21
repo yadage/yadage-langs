9200612e4a8d231cca615974fd3b84435f32fd5c
lukasheinrich <lukas.heinrich@gmail.com>
2018-06-22 00:14:19 +0200

    extreme

--- simple/simple.py
+++ simple/simple.py
@@ -26,6 +26,8 @@ class dag():
         return selection(pointer(expr))
 
 class state():
+    def __getitem__(self,key):
+        return self.join(key)
     def join(self,path):
         return '/'.join(['{workdir}',path])
 
@@ -75,7 +77,7 @@ def step(func):
     return {
         'process': {
             'process_type': 'interpolated-script-cmd',
-            'script': cfg.cmd.format(pars = pars)
+            'script': cfg.cmd.format(inp = cfg.inp, workdir = cfg.wrk)
         },
         'environment': {
             'environment_type': 'docker-encapsulated',
@@ -86,6 +88,22 @@ def step(func):
             'publish': pub
         }
     }
+
+class worktracker(object):
+    def __init__(self,work):
+        self.workdir = work
+        self.tracked = []
+    def __getitem__(self,key):
+        self.tracked.append(self.workdir.join(key))
+        return '{{wrk{}}}'.format(len(self.tracked)-1)
+
+class inputtracker(object):
+    def __init__(self,scope):
+        self.scope = scope
+        self.tracked = []
+    def __getitem__(self,key):
+        self.tracked.append(eval('self.scope.{}'.format(key)))
+        return '{{arg{}}}'.format(len(self.tracked)-1)
 
 class stage(object):
     def parameters(self, **kwargs):
@@ -113,8 +131,6 @@ def singlestep(step,image,name):
 #--------------------------------
 
 workdir, scope = maker()
-
-
 from yadageschemas.dialects import dialect
 from yadageschemas.dialects.raw_with_defaults import extend_with_defaults
 import importlib
@@ -135,16 +151,24 @@ def dagdowndialect(spec,specopts):
     d = yaml.load(open(spec))
     w = wflow(d.pop('environment',None))
     for k,x in d.items():
+        tracker  = inputtracker(scope)
+        wrktrack = worktracker(workdir)
         @step
         def __step(s,pars):
             s.cmd = x['cmd']
+            s.inp = tracker
+            s.wrk = wrktrack
             if 'environment' in x:
                 s.image = x['environment']
             return {k:eval_better(v,{}, {'pars': pars,'workdir': workdir}) for k,v in x['output'].items()}
         @w.singlestep(__step,k)
         def __runstep(s):
-            s.parameters(**{k: eval_better(v) for k,v in x['input'].items()})
+            pars = {}
+            for i,t in enumerate(tracker.tracked):
+                pars['arg{}'.format(i)] = t
+            for i,t in enumerate(wrktrack.tracked):
+                pars['wrk{}'.format(i)] = t
+            s.parameters(**pars)
     data = json.loads(w.compile())
     extend_with_defaults(data, specopts['schema_name'], specopts['schemadir'])
     return data
-
